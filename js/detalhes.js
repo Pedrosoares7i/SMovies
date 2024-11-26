25bca6881d12de4a828fb648ee5a82f4b6f101e2
Pedrosoares7i <phs087414.cxj@gmail.com>
2024-11-26 16:44:15 -0300

    Foi adicionado a parte dos elencos detalhes do filme e também sobre o desenvolvedor do site.

--- js/detalhes.js
+++ js/detalhes.js
@@ -34,7 +34,46 @@ async function getMovie() {
     <p>${movie.overview}</p>`;
 
 
-    movie.genres.forEach (genre => { 
+    movie.genres.forEach(genre => {
         detalhes.innerHTML += `<button class='btn btn-lg btn-outline-danger me-2'>${genre.name}</button>`
     })
+
+    // Buscar o trailer do filme
+    let trailer;
+    await fetch(`${baseUrl}${media}/${id}/videos?language=pt-br`, options)
+        .then(res => res.json())
+        .then(res => trailer = res.results)
+        .catch(err => console.log('Erro ao carregar trailer', err));
+    // console.log(trailer);
+    if (trailer.length > 0) {
+        document.querySelector('iframe').src = `https://www.youtube.com/embed/${trailer[0].key}`
+    } else {
+        document.querySelector('#trailer').style.display = 'none';
+    }
+
+    // Começo do elenco
+    let elenco;
+    await fetch(`${baseUrl}${media}/${id}/credits?language=pt-br`, options)
+        .then(res => res.json())
+        .then(res => elenco = res.cast)
+        .catch(err => console.log('Erro ao carregar elenco', err));
+    // console.log(elenco);
+
+    let elencoContainer = document.getElementById('elenco');
+    elencoContainer.innerHTML = '';
+    for (let i = 0; i < elenco.length; i++) {
+        let foto = elenco[i].profile_path ? `https://image.tmdb.org/t/p/original/${elenco[i].profile_path}` : 'img/no-photo-cast.png';
+        elencoContainer.innerHTML += 
+            `<div class = 'col-lg-4 col-sm-6'>
+                <div class ='row'>
+                    <div class ='col-lg-3 mb-3'>
+                        <img class= "w-100 rounded-3" src="${foto}">
+                    </div>
+                    <div class='col-lg-9 mb-3'>
+                    <h4>${elenco[i].original_name}</h4>
+                    <p>${elenco[i].character}</p>
+                    </div>
+                </div>
+            </div>`;
+    }
 }
